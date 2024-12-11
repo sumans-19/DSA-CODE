dd398656131eb5258f166b39d2c317d661ccf7c5
Suman S <147817337+sumans-19@users.noreply.github.com>
2024-12-11 11:23:45 +0530

    Update and rename PROGRAM-1 to PROGRAM-1.c

--- PROGRAM-1.c
+++ PROGRAM-1.c
@@ -1,15 +1,15 @@
-1) Design, Develop and Implement a Program in C for the following operations on
-Strings
-a. Read a main String (STR), a Pattern String (PAT) and a Replace String (REP)
-b. Perform Pattern Matching Operation: Find and Replace all occurrences of PAT in STR
-with REP if PAT exists in STR. Report suitable messages in case PAT does not exist in STR.
-c. Pattern Matching Algorithm: Brute Force
-d. Support the program with functions for each of the above operations. Don't use Builtin functions
-e. Check the following test cases.
-Test Case 1: STR = “VVCE MYSURU”, PAT=” MYSURU”, REP=” KARNATAKA”, OUTPUT=”
-VVCE KARNATAKA”
-Test Case 2: STR = “COMPUTER SCIENCE”, PAT=” COMPUTER”, REP=” BASIC”,
-OUTPUT=” BASIC SCIENCE”
+// 1) Design, Develop and Implement a Program in C for the following operations on
+// Strings
+// a. Read a main String (STR), a Pattern String (PAT) and a Replace String (REP)
+// b. Perform Pattern Matching Operation: Find and Replace all occurrences of PAT in STR
+// with REP if PAT exists in STR. Report suitable messages in case PAT does not exist in STR.
+// c. Pattern Matching Algorithm: Brute Force
+// d. Support the program with functions for each of the above operations. Don't use Builtin functions
+// e. Check the following test cases.
+// Test Case 1: STR = “VVCE MYSURU”, PAT=” MYSURU”, REP=” KARNATAKA”, OUTPUT=”
+// VVCE KARNATAKA”
+// Test Case 2: STR = “COMPUTER SCIENCE”, PAT=” COMPUTER”, REP=” BASIC”,
+// OUTPUT=” BASIC SCIENCE”
 
 CODE :
 #include <stdio.h>
