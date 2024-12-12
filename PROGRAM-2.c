a74b754c76b8a6f8d8f4635ed416eb726d5374cc
Suman S <147817337+sumans-19@users.noreply.github.com>
2024-12-12 14:17:45 +0530

    Update and rename PROGRAM-2 to PROGRAM-2.c

--- PROGRAM-2.c
+++ PROGRAM-2.c
@@ -1,13 +1,13 @@
- 2) Design, Develop and Implement a menu driven Program in C for the following
-operations on STACK of Integers (Array Implementation of Stack with maximum size
-MAX)
-a. Push an Element on to Stack
-b. Pop an Element from Stack
-c. Demonstrate how Stack can be used to check Palindrome
-d. Demonstrate Overflow and Underflow situations on Stack
-e. Display the status of Stack
-f. Exit
-Support the program with appropriate functions for each of the above operations 
+//  2) Design, Develop and Implement a menu driven Program in C for the following
+// operations on STACK of Integers (Array Implementation of Stack with maximum size
+// MAX)
+// a. Push an Element on to Stack
+// b. Pop an Element from Stack
+// c. Demonstrate how Stack can be used to check Palindrome
+// d. Demonstrate Overflow and Underflow situations on Stack
+// e. Display the status of Stack
+// f. Exit
+// Support the program with appropriate functions for each of the above operations 
 
 CODE : 
 #include <stdio.h>
